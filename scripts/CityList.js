88eb23077c5dcb41c51433e79fabc3156933ccc4
Sara Hackle <Shackle@ivytech.edu>
2021-12-01 14:13:27 -0600

    added cities to database, refactored rest of code to display city properly

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,14 +1,15 @@
 //imports walker objects from array in database
-import { getWalkers } from "./database.js";
+import { getWalkers, getCities } from "./database.js";
 
 const walkers = getWalkers();
+const cities = getCities();
 
 //exports functions the builds the html list of cities from properties of walkers to main.js
 export const CityList = () => {
   let citiesHTML = "<ol>";
 
-  for (const walker of walkers) {
-    citiesHTML += `<li>${walker.city}</li>`;
+  for (const city of cities) {
+    citiesHTML += `<li "id= city--${city.id}">${city.name}</li>`;
   }
 
   citiesHTML += "</ol>";
--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,21 +1,25 @@
 //imports list of walkers array from database.js
-import { getWalkers } from "./database.js";
+import { getWalkers, getCities } from "./database.js";
 
 //added first event listener, on click event it shows the city that the walker services in an alert box
 document.addEventListener("click", (clickEvent) => {
   const itemClicked = clickEvent.target;
   if (itemClicked.id.startsWith("walker")) {
     const [, walkerId] = itemClicked.id.split("--");
-
-    for (const walker of walkers) {
-      if (walker.id === parseInt(walkerId)) {
-        window.alert(`${walker.name} services ${walker.city}`);
-      }
-    }
+    const clickedWalker = walkers.find(
+      (walker) => walker.id === parseInt(walkerId)
+    );
+    const clickedCity = cities.find(
+      (walker) => walker.id === clickedWalker.cityId
+    );
+    window.alert(
+      `${clickedWalker.name} services the city of ${clickedCity.name}`
+    );
   }
 });
 
 const walkers = getWalkers();
+const cities = getCities();
 
 //exports function that builds html for a list of walker names to main.js
 export const Walkers = () => {
